23d12a57bf4a78519b24019d6dbcb758e7fea997
Marin Garcia, Jesus C <jesus.c.marin.garcia@accenture.com>
2017-05-29 15:09:00 -0500

    Adding new updates

--- jenkins/jobs/dsl/doa17_s3_site_cartridge.groovy
+++ jenkins/jobs/dsl/doa17_s3_site_cartridge.groovy
@@ -37,7 +37,7 @@ doa17PullCode.with{
     stringParam("S3_BUCKET",doa17BucketName,"AWS S3 Bucket Name")
     stringParam("AWS_REGION",'',"AWS Region")
     stringParam("AWS_ACCESS_KEY",'',"AWS Access Key")
-    stringParam("AWS_SECRET_KEY",'',"AWS Secret Key")
+    nonStoredPasswordParam("AWS_SECRET_KEY","AWS Secret Key")
   }
   wrappers {
     preBuildCleanup()
@@ -45,6 +45,23 @@ doa17PullCode.with{
     sshAgent("adop-jenkins-master")
   }
   label("docker")
+  triggers {
+   gerrit {
+     events {
+       refUpdated()
+     }
+     project("${PROJECT_NAME}/" + siteRepoName, 'master')
+    }
+  }
+  scm{
+     git{
+       remote{
+         url(siteRepoUrl)
+         credentials("adop-jenkins-master")
+       }
+       branch("*/master")
+     }
+  }
   publishers{
     downstreamParameterized{
       trigger(projectFolderName + "/DOA17_Create_Bucket"){
@@ -68,13 +85,35 @@ doa17CreateBucket.with{
     stringParam("S3_BUCKET",'',"AWS S3 Bucket Name")
     stringParam("AWS_REGION",'',"AWS Region")
     stringParam("AWS_ACCESS_KEY",'',"AWS Access Key")
-    stringParam("AWS_SECRET_KEY",'',"AWS Secret Key")
+    nonStoredPasswordParam("AWS_SECRET_KEY",'',"AWS Secret Key")
   }
   wrappers {
     preBuildCleanup()
     maskPasswords()
   }
   label("docker")
+    steps {
+    shell('''
+set +x
+
+echo "[LOG] Export AWS Configuration"
+export AWS_ACCESS_KEY_ID=$AWS_ACCES_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+
+echo "[LOG] Create S3 Bucket $S3_BUCKET if not exists"
+S3_BUCKETS=$(aws s3 ls)
+if grep -q $S3_BUCKET <<<$S3_BUCKETS; then
+  echo "[LOG] S3 Bucket $S3_BUCKET already exists"
+else
+  echo "[LOG] Creating S3 Bucket $S3_BUCKET"
+  aws s3 mb s3://$S3_BUCKET
+fi
+
+set -x'''.stripMargin()
+    )
+  }
   publishers{
     downstreamParameterized{
       trigger(projectFolderName + "/DOA17_Deploy_Site"){
@@ -98,11 +137,37 @@ doa17DeploySite.with{
     stringParam("S3_BUCKET",'',"AWS S3 Bucket Name")
     stringParam("AWS_REGION",'',"AWS Region")
     stringParam("AWS_ACCESS_KEY",'',"AWS Access Key")
-    stringParam("AWS_SECRET_KEY",'',"AWS Secret Key")
+    nonStoredPasswordParam("AWS_SECRET_KEY",'',"AWS Secret Key")
   }
   wrappers {
     preBuildCleanup()
     maskPasswords()
   }
   label("docker")
+  steps {
+    copyArtifacts(projectFolderName + "/FIME_Pull_Code") {
+      buildSelector {
+        latestSuccessful(true)
+      }
+    }
+    shell('''
+set +x
+
+echo "[LOG] Export AWS Configuration"
+export AWS_ACCESS_KEY_ID=$AWS_ACCES_KEY
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+export AWS_DEFAULT_REGION=$AWS_REGION
+
+echo "[LOG] Deploy Site to AWS S3 Bucket"
+aws s3 cp index.html s3://$S3_BUCKET/index.html --acl public-read
+
+echo "[LOG] Create Website from S3 Bucket"
+aws s3 website s3://$S3_BUCKET/ --index-document index.html
+
+echo "[LOG] Check your new site at:"
+echo "http://$S3_BUCKET.s3-website-$AWS_REGION.amazonaws.com"
+
+set -x'''.stripMargin()
+    )
+  }
 }
